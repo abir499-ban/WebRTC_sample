2da9e7be0fcea9cbd360e72f9fb8f2ed351b68d1
Abir Banerjee <abirbanerjee793@gmail.com>
2024-12-24 18:29:10 +0530

    re render fixed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,45 +1,50 @@
-import {useContext, useEffect, useMemo, useState} from 'react'
+import { useContext, useEffect, useMemo, useState, useCallback } from 'react'
 import { useSocketContext } from '../hook/useSocketContext'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const navigate = useNavigate();
-    const {socket} = useSocketContext();
-      const [Roominfo, setRoominfo] = useState({
-        emailID : "",
-        roomID : "",
-      })
-      const connectSocket  = (e) =>{
-        e.preventDefault();
-        socket.emit('join_room', Roominfo);
-      }
-    
-    
-      useEffect(()=>{
-        socket.on('joined_room', ({roomID}) =>{
-            console.log('Joined Room ', roomID)
-            navigate(`/room/${roomID}`);
-        });
-      }, [socket])
-      
+  const navigate = useNavigate();
+  const { socket } = useSocketContext();
+  const [Roominfo, setRoominfo] = useState({
+    emailID: "",
+    roomID: "",
+  })
+  const connectSocket = (e) => {
+    e.preventDefault();
+    socket.emit('join_room', Roominfo);
+  }
+
+  const handleRoomJoin = useCallback(({ roomID }) => {
+    console.log('Joined Room ', roomID)
+    navigate(`/room/${roomID}`);
+  }, [navigate])
+
+  useEffect(() => {
+    socket.on('joined_room', handleRoomJoin);
+
+    return () => {
+      socket.off('joined_room', handleRoomJoin)
+    }
+  }, [handleRoomJoin, socket])
+
   return (
     <>
-    <div className='container mx-auto  text-center flex flex-wrap flex-col justify-center items-center gap-3'>
-      <h1 className='text-center p-6 font-normal text-6xl '>Welcome to Web RTC</h1>
-      <input placeholder='Enter Email ID' className='border-2 border-gray-700 rounded-md p-4'
-      value={Roominfo.emailID}
-      onChange={(e)=>setRoominfo((prev) =>({
-        ...prev,
-        emailID : e.target.value
-      }))}></input>
-      <input placeholder='Enter Room ID' className='border-2 border-gray-700 rounded-md p-4'
-      value={Roominfo.roomID}
-      onChange={(e)=>setRoominfo((prev) =>({
-        ...prev,
-        roomID : e.target.value
-      }))}></input>
-      <button className='bg-blue-400 text-white rounded-md p-2 hover:bg-blue-600' 
-      onClick={(e) => connectSocket(e)}>Click Me to connect</button>
+      <div className='container mx-auto  text-center flex flex-wrap flex-col justify-center items-center gap-3'>
+        <h1 className='text-center p-6 font-normal text-6xl '>Welcome to Web RTC</h1>
+        <input placeholder='Enter Email ID' className='border-2 border-gray-700 rounded-md p-4'
+          value={Roominfo.emailID}
+          onChange={(e) => setRoominfo((prev) => ({
+            ...prev,
+            emailID: e.target.value
+          }))}></input>
+        <input placeholder='Enter Room ID' className='border-2 border-gray-700 rounded-md p-4'
+          value={Roominfo.roomID}
+          onChange={(e) => setRoominfo((prev) => ({
+            ...prev,
+            roomID: e.target.value
+          }))}></input>
+        <button className='bg-blue-400 text-white rounded-md p-2 hover:bg-blue-600'
+          onClick={(e) => connectSocket(e)}>Click Me to connect</button>
       </div>
     </>
   )
--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -1,32 +1,38 @@
 import React, { useCallback, useEffect } from 'react'
-import {useSocketContext} from '../hook/useSocketContext'
-import {usePeer} from '../context/Peercontext'
+import { useSocketContext } from '../hook/useSocketContext'
+import { usePeer } from '../context/Peercontext'
 
 const Room = () => {
-    const {socket}  = useSocketContext();
-    const {peer, createOffer} = usePeer();
+  const { socket } = useSocketContext();
+  const { peer, createOffer } = usePeer();
 
-    const handleUserJoin =  useCallback(
-      async(data) =>{
-        const {emailID} = data;
-        console.log("User joined ", emailID);
-        const offer = await createOffer();
-        socket.emit('call_user', {emailID, offer})
-      },
-      [createOffer, socket]
-    )
-
-    const handleincomingCall  = useCallback(({from, offer})=>{
-      console.log("incoming call from ", from, offer)
-    }, [])
-
-    useEffect(()=>{
-        socket.on('user_joined', handleUserJoin)
-        socket.on('incoming_call', handleincomingCall);
-    }, [handleUserJoin,socket])
-  return (
-    <div>Room</div>
+  const handleUserJoin = useCallback(
+    async (data) => {
+      const { emailID } = data;
+      console.log("User joined ", emailID);
+      const offer = await createOffer();
+      socket.emit('call_user', { emailID, offer })
+    },
+    [createOffer, socket]
   )
+
+  const handleincomingCall = useCallback(({ from, offer }) => {
+    console.log("incoming call from ", from, offer)
+  }, [])
+
+  useEffect(() => {
+    socket.on('user_joined', handleUserJoin)
+    socket.on('incoming_call', handleincomingCall);
+
+    return () => {
+      socket.off('user_joined', handleUserJoin);
+      socket.off('incoming_call', handleincomingCall)
+    }
+        
+}, [handleUserJoin, handleincomingCall, socket])
+return (
+  <div>Room</div>
+)
 }
 
 export default Room
